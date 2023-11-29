9b0868282e073c8d736c5cc1329880d322a90ecc
nas <nasissa97@gmail.com>
2023-11-29 07:54:35 -0800

    Create Type Constraint KeyValuePair2 template class

--- src/templates/KeyValuePair.cpp
+++ src/templates/KeyValuePair.cpp
@@ -1,7 +1,10 @@
 #ifndef KEYVALUEPAIR_HPP
 #define KEYVALUEPAIR_HPP
 
+#include <iostream>
+#include <string>
 #include <utility>
+
 
 namespace templates {
 template<typename K, typename V>
@@ -9,7 +12,9 @@ class KeyValuePair {
  public:
   KeyValuePair(K key, V value)
    : m_key{ std::move(key) }, m_value{ std::move(value) }
-  {}
+  {
+    std::cout << "Regular Template Constructor" << std::endl;
+  }
 
  
   /** Getter and Setter for m_key */
@@ -33,6 +38,39 @@ class KeyValuePair {
  private:
   K m_key;
   V m_value;
+};
+
+template<>
+class KeyValuePair<const char*, const char*> {
+ public:
+  KeyValuePair(const char* key, const char* value)
+   : m_key{ std::move(key) }, m_value{ std::move(value) }
+  {
+    std::cout << "Specialization Template Constructor" << std::endl;
+  }
+
+ 
+  /** Getter and Setter for m_key */
+  auto getKey() {
+   return m_key;
+  }
+
+  void setKey(std::string key) {
+   m_key = key;
+  }
+
+  /** Getter and Setter for m_value */
+  auto getValue() {
+   return m_value;
+  }
+
+  void setValue(std::string value) {
+   m_value = value;
+  }
+
+ private:
+  std::string m_key;
+  std::string  m_value;
 };
 };
 
---
+++ src/templates/KeyValuePair2.cpp
@@ -0,0 +1,45 @@
+#ifndef KEYVALUEPAIR2_HPP
+#define KEYVALUEPAIR2_HPP
+
+#include <concepts>
+#include <iostream>
+#include <string>
+#include <utility>
+
+
+namespace templates {
+template<std::integral K, std::floating_point V>
+class KeyValuePair2 {
+ public:
+  KeyValuePair2(K key, V value)
+   : m_key{ std::move(key) }, m_value{ std::move(value) }
+  {
+    std::cout << "Regular Template Constructor" << std::endl;
+  }
+
+ 
+  /** Getter and Setter for m_key */
+  auto getKey() {
+   return m_key;
+  }
+
+  void setKey(K key) {
+   m_key = key;
+  }
+
+  /** Getter and Setter for m_value */
+  auto getValue() {
+   return m_value;
+  }
+
+  void setValue(V value) {
+   m_value = value;
+  }
+
+ private:
+  K m_key;
+  V m_value;
+};
+};
+
+#endif
